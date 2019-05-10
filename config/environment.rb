adb944e124ddf7b357e34242419d6f9074f52117
torianne02 <victoria.fluharty@gmail.com>
2019-05-10 16:31:09 -0700

    updates environment.rb to accept postgres db adds pizza-analytics_development db to file after error

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,19 @@
+ENV['SINATRA_ENV'] ||= "development"
+
 require 'bundler'
+Bundler.require(:default, ENV['SINATRA_ENV'])
 
-bundler.require
+configure :production do
+ db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/pizza-analytics')
 
-ActiveRecord::Base.establish_connection(
-  :adapter => 'postgresql',
-  :database => 'pizza-analytics_development'
-)
+ ActiveRecord::Base.establish_connection(
+   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+   :host     => db.host,
+   :username => db.user,
+   :password => db.password,
+   :database => db.path[1..-1],
+   :encoding => 'utf8'
+ )
+end
 
 require_all 'app'
