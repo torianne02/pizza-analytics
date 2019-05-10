07273dcb1a7ac3fbf57cdcfad37cad0831dd74ab
torianne02 <victoria.fluharty@gmail.com>
2019-05-10 16:24:34 -0700

    changes datetime to date for :date_consumed in pizzas table

--- db/migrate/20190510231131_create_pizzas.rb
+++ db/migrate/20190510231131_create_pizzas.rb
@@ -2,7 +2,7 @@ class CreatePizzas < ActiveRecord::Migration[5.2]
   def change
     create_table :pizzas do |t|
       t.string :type
-      t.datetime :date_consumed
+      t.date :date_consumed
       t.integer :person_id
     end
   end
