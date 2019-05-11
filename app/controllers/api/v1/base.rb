6d49084aa2c9f8a6bdebb19ecc47f83457a70a5c
torianne02 <victoria.fluharty@gmail.com>
2019-05-10 20:51:14 -0700

    adds swagger documentation to api/v1 base file

--- app/controllers/api/v1/base.rb
+++ app/controllers/api/v1/base.rb
@@ -1,8 +1,17 @@
+require 'grape-swagger'
+
 module API
   module V1
     class Base < Grape::API
       mount API::V1::People
       mount API::V1::Pizzas
+
+      add_swagger_documentation(
+        api_version: "v1",
+        hide_documentation_path: true,
+        mount_path: '/api/v1/swagger_doc',
+        hide_format: true
+      )
     end
   end
-end 
+end
