1cf73e2b9b238cbbdf53e11bd14f4d295a158ebc
torianne02 <victoria.fluharty@gmail.com>
2019-05-10 20:05:16 -0700

    creates pizzas.rb within api/v1 adds comments for future tasks

---
+++ app/controllers/api/v1/pizzas.rb
@@ -0,0 +1,28 @@
+module API
+  module V1
+    class Pizzas < Grape::API
+      include API::V1::Defaults
+
+      resource :pizzas do
+        # returns ALL pizzas
+        desc "Return all pizza"
+        get "", root: :pizzas do
+          Pizzas.all
+        end
+
+        # returns ALL pizzas BY topping
+        desc "Return all pizzas of a certain topping"
+        params do
+          requires :type, type: String, desc: "Type of topping"
+        end
+        get ":type", root: "pizzas" do
+          Pizzas.where(type: permitted_params[:type]).first!
+        end
+
+        # need to figure out how to sum all pizzas eaten on each day
+        # then need to figure out how many consecutive days more pizzas have been
+          # consumed than the day before
+      end
+    end
+  end
+end
