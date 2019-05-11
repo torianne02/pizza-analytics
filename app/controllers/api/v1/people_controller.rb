07f18ff48081e9f8a794507a78d2a0babd691858
torianne02 <victoria.fluharty@gmail.com>
2019-05-11 08:34:00 -0700

    creates people_controller.rb adds logic for routes

---
+++ app/controllers/api/v1/people_controller.rb
@@ -0,0 +1,17 @@
+module API
+  module V1
+    class PeopleController < ApplicationController
+      # GET all people
+      get '/people' do
+        @people = People.all
+        render json: @people
+      end
+
+      # GET person by name
+      get '/people/:name' do
+        @person = Trip.find_by(name: params[:name])
+        render json: @person
+      end
+    end
+  end
+end
