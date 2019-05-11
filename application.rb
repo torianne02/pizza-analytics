0f716ba9733edb71a50de78d351a4c4b5d89a8b8
torianne02 <victoria.fluharty@gmail.com>
2019-05-11 14:06:59 -0700

    adds logic to application.rb mounts grape application established db connection creates API module class with Root imports model and api files

--- application.rb
+++ application.rb
@@ -1,4 +1,6 @@
 require 'grape'
+require 'active_record'
+require 'sinatra'
 
 # loading files from the models and api folders
 Dir["#{File.dirname(__FILE__)}/app/models/**/*.rb"].each { |f| require f }
@@ -16,8 +18,25 @@ module API
   end
 end
 
-# mount Grape application
+ENV['SINATRA_ENV'] ||= "development"
 
+require 'bundler'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+
+# establish ActiveRecord connection to db
+configure :production do
+ db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/pizza-analytics')
+
+ ActiveRecord::Base.establish_connection(
+
+   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+   :host     => db.host,
+   :database => db.path[1..-1]+'_test',
+   :encoding => unicode
+ )
+end
+
+# mount Grape application
 PizzaAnalytics = Rack::Builder.new {
   map "/" do
     run API::Root
