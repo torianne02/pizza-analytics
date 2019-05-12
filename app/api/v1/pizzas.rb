7b96835a32ba556f9eabff1e327798a8fe37ab1d
torianne02 <victoria.fluharty@gmail.com>
2019-05-12 15:06:15 -0700

    creates sequel queries for /streaks and /monthly-sales

--- app/api/v1/pizzas.rb
+++ app/api/v1/pizzas.rb
@@ -1,12 +1,9 @@
-require 'pry'
-
 module API
   module V1
     class Pizzas < Grape::API
       version 'v1', using: :path
 
       resources :pizzas do
-        # returns ALL pizzas
         desc 'Return all pizza'
         get '/' do
           # if topping params exists - find pizzas by topping
@@ -16,6 +13,53 @@ module API
           else
             Pizza.all
           end
+        end
+
+        desc 'Return day with highest sales of month'
+        get '/monthly-sales' do
+          @data = DB["WITH day_month_sales as (
+
+            SELECT date_consumed, count(*) num_sales
+              , to_char(date_consumed, 'YYYY-MM') month_year
+            FROM pizzas
+            GROUP BY date_consumed
+              , to_char(date_consumed, 'YYYY-MM')
+          )
+
+          , max_sales_in_day_per_month as (
+
+            SELECT month_year, MAX(num_sales) max_sales_in_month
+            FROM day_month_sales
+            GROUP BY month_year
+          )
+
+            SELECT date_consumed, num_sales
+            FROM day_month_sales as dms
+              INNER JOIN max_sales_in_day_per_month as msdpm
+                ON dms.month_year = msdpm.month_year
+                AND dms.num_sales = msdpm.max_sales_in_month
+            ORDER BY date_consumed"]
+          return @data
+        end
+
+        desc 'Return streaks of days that are > the day prior'
+        get '/streaks' do
+          @data = DB["
+            WITH daily_pizzas as (
+                SELECT date_consumed
+                  , count(1) num_pizzas
+                  , ROW_NUMBER() OVER (ORDER BY date_consumed) AS rank
+                FROM pizzas
+                GROUP BY date_consumed
+            )
+
+            select curr_day.date_consumed, curr_day.num_pizzas
+            from daily_pizzas curr_day
+              left join daily_pizzas prev_day
+                on curr_day.rank = prev_day.rank + 1
+             WHERE curr_day.num_pizzas > coalesce(prev_day.num_pizzas,0)
+            order by curr_day.date_consumed, prev_day.date_consumed"]
+          return @data
         end
 
         # returns ALL pizzas BY topping
