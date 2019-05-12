e970d0caec693589edcc45b0574f64e3031ce77f
torianne02 <victoria.fluharty@gmail.com>
2019-05-12 10:12:02 -0700

    creates validation methods for models

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -1,5 +1,9 @@
 class Pizza < Sequel::Model
   many_to_one :person
 
-  # validates :topping, presence: true
+  def validate
+    super
+    errors.add(:topping, "must be present") if topping.empty?
+    errors.add(:date_consumed, "must be present") if date_consumed.empty?
+  end
 end
