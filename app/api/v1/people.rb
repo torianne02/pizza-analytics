3e2ee0de553b86a87640b3451ee9ce76ca8c5660
torianne02 <victoria.fluharty@gmail.com>
2019-05-12 13:40:25 -0700

    changes logic to accept name params for get request in people.rb

--- app/api/v1/people.rb
+++ app/api/v1/people.rb
@@ -4,19 +4,15 @@ module API
       version 'v1', using: :path
 
       resource :people do
-        # returns ALL people
         desc 'Return all people'
         get '/' do
-          Person.all
-        end
-
-        # returns ONE person by NAME
-        desc 'Return a person'
-        params do
-          requires :name, type: String, desc: 'Name of the person'
-        end
-        get ':name' do
-          Person.find(params[:name])
+          # returns person by name if name param is provided
+          if params.include?("name")
+            Person.where(name: params[:name]).first!
+          # returns all people if there is no name param
+          else
+            Person.all
+          end
         end
       end
     end
