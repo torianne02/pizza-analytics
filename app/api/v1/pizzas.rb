9b46048ec07a97a0b00dd38d2e6c6125337fd021
torianne02 <victoria.fluharty@gmail.com>
2019-05-12 13:41:51 -0700

    updates logic in pizza endpoint to accept params for topping in get request

--- app/api/v1/pizzas.rb
+++ app/api/v1/pizzas.rb
@@ -1,23 +1,31 @@
+require 'pry'
+
 module API
   module V1
     class Pizzas < Grape::API
       version 'v1', using: :path
 
-      resource :pizzas do
+      resources :pizzas do
         # returns ALL pizzas
         desc 'Return all pizza'
         get '/' do
-          Pizza.all
+          # if topping params exists - find pizzas by topping
+          if params.include?("topping")
+            Pizza.where(topping: params[:topping])
+          # if no params return all pizzas
+          else
+            Pizza.all
+          end
         end
 
         # returns ALL pizzas BY topping
-        desc 'Return all pizzas of a certain topping'
-        params do
-          requires :topping, type: String
-        end
-        get ':topping' do
-          Pizza.where(topping: params[:topping])
-        end
+        # desc 'Return all pizzas of a certain topping'
+        # params do
+        #   requires :topping, type: String
+        # end
+        # get ':topping' do
+        #   Pizza.where(topping: params[:topping])
+        # end
 
         # need to figure out how to sum all pizzas eaten on each day
         # then need to figure out how many consecutive days more pizzas have been
