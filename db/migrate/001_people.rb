58126b3588f4f9d8a318f0fec42e273cf30e77db
torianne02 <victoria.fluharty@gmail.com>
2019-05-22 20:07:57 -0700

    adds commented out code a separate db was created to test CSV file data - code is commented out

--- db/migrate/001_people.rb
+++ db/migrate/001_people.rb
@@ -2,7 +2,7 @@ Sequel.migration do
   change do
     create_table(:people) do
       primary_key :id
-      String :name, :null => false
+      String :name, :null => false, unique: true
     end
-  end 
+  end
 end
